94cce1560b7d054e94b4bd656a9b0f3e6b347b71
Nikhil Kadiyan <kadiyannikhil2@gmail.com>
2023-10-26 19:26:19 +0530

    admin login checked

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const saltRounds=10;
 
-const port=3000;
+mongoose.connect('mongodb://127.0.0.1:27017/ResultDB');
+
+const port=4000;
 const app = express();
+
+const adminSchema = {
+    email: String,
+    password: String
+}
+
+const Admin = mongoose.model('Admin',adminSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
-});
-
-app.post("/admin",(req,res)=>{
-    console.log("admin logined");
-});
-
-app.post("/student",(req,res)=>{
-    console.log("student login");
+app.post("/adminlogin", (req,res)=>{
+    console.log(req.body);
+    Admin.findOne({email: req.body.email}).then((data)=>{
+        if(data !== null){
+            bcrypt.compare(req.body.password, data.password, function(err, result) {
+                if(result === true){
+                    const ans= true;
+                    res.json(ans);
+                }else{
+                    const ans= false;
+                    res.json(ans);
+                }
+            });
+        }else{
+            const ans= false;
+            res.json(ans);
+        }
+    });
 });
 
 
--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
-const port=4000;
+const port=3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+const APIurl = "http://localhost:4000"
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/api/admin",async (req,res)=> {
+    try{
+        console.log(req.body);
+        const reponse = await axios.post(`${APIurl}/adminlogin`, req.body);
+        console.log(reponse.data);
+    }catch(error){
+        console.error("Failed to make request:",error.message);
+        res.status(500).send("failed to fetch activity. Please try again.");
+    }
+});
+
+app.post("/student",(req,res)=>{
+    console.log("student login");
+});
 
 
 app.listen(port,function(){
